401bcfde8278ae8741e8c8bc7a2fe6180de1a03f
Brandon griger <bgriger@Brandons-MacBook-Air.local>
2021-09-16 15:38:07 -0500

    added logic to display recipe when cocktail is clicked:

--- Cocktail/cocktail.js
+++ Cocktail/cocktail.js
@@ -1,6 +1,8 @@
 let selection = document.getElementById('booze');
 let list = document.getElementById('list');
+let recipe = document.getElementById('recipe');
 selection.addEventListener('change', ()=>{
+    recipe.innerHTML = '';
     let value = selection.options[selection.selectedIndex].value;
     if(value != 'default'){
         list.innerHTML = '';
@@ -9,7 +11,7 @@ selection.addEventListener('change', ()=>{
             .then(response => response.json()
             ).then(jsonResponse =>{
                 console.log(jsonResponse);
-                for(let x = 0; x < 5; x++){
+                for(let x = 0; x < 3; x++){
                     let randomIndex = Math.floor(Math.random() * (jsonResponse.drinks.length - 1))
                     let name = jsonResponse.drinks[randomIndex].strDrink;
                     let imgUrl = jsonResponse.drinks[randomIndex].strDrinkThumb + '/preview';
@@ -23,8 +25,33 @@ selection.addEventListener('change', ()=>{
 
                 for(let i = 0; i < links.length; i++){
                     links[i].addEventListener("click",() =>{
+                        recipe.innerHTML = '';
                         const drinkId = links[i].alt;
-                        console.log(drinkId);
+                        const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + drinkId;
+                        fetch(url)
+                            .then(response => response.json()
+                            ).then(jsonResponse =>{
+                                console.log(jsonResponse);
+                                let container = document.createElement('div');
+                                container.classList.add('recipe');
+
+                                for(let x = 1; x < 16; x++){
+                                    let ingredient = jsonResponse.drinks[0]['strIngredient' + x];
+                                    let measure = jsonResponse.drinks[0]['strMeasure' + x];
+                                    if(!measure){
+                                        measure = 'To taste';
+                                    }
+                                    container.innerHTML += `${ingredient} - ${measure}<br />`;
+                                    if(!jsonResponse.drinks[0]['strIngredient' + (x + 1)]){
+                                        break;
+                                    }
+                                }
+                                container.innerHTML += `<br/>${jsonResponse.drinks[0].strInstructions}`;
+                                recipe.appendChild(container);
+
+
+                                
+                            })
                     })
                 }
             }) 
